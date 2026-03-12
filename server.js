8342cbfabaa36d3913faaf566385603584a8f567
Manish Bisht <manibisht345@gmail.com>
2026-03-12 12:00:01 +0530

    fix orgin

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-     origin: "https://codecollab-gamma.vercel.app",
+    origin: [process.env.FRONTEND_URL, "https://codecollab-gamma.vercel.app", "http://localhost:5173"].filter(Boolean),
     methods: ["GET", "POST"],
     credentials: true
   }
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 // socket logic
 socketHandler(io);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
