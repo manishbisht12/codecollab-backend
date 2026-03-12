9f47f64e1d02baa8868e35d47432423cbd0dd25a
Manish Bisht <manibisht345@gmail.com>
2026-03-12 11:46:30 +0530

    orgin use  url

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
+     origin: "https://codecollab-gamma.vercel.app",
+    methods: ["GET", "POST"],
+    credentials: true
   }
 });
 
